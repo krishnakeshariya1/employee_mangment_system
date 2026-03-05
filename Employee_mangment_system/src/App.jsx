7da8036c2858a5fe10b37ea92e6fd46c0ff99fe4
krishnakeshariya1 <keshariyakrishna8@gmail.com>
2026-03-05 19:02:43 +0530

    add TaskList

--- Employee_mangment_system/src/App.jsx
+++ Employee_mangment_system/src/App.jsx
@@ -3,10 +3,10 @@ import { EmployeeDashboard } from "./Components/Dashboard/EmployeeDashboard"
 
 const App =()=>{
   return(
-    <>
+    < div className="w-screen">
     {/* < Login /> */}
     < EmployeeDashboard />
-    </>
+    </ div>
   )
 }
 export default App
