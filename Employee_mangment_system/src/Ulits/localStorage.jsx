d52294f478a25105bf717aaa124071c7f4a25bcc
krishnakeshariya1 <keshariyakrishna8@gmail.com>
2026-03-07 17:32:19 +0530

    add employees and admin data

---
+++ Employee_mangment_system/src/Ulits/localStorage.jsx
@@ -0,0 +1,34 @@
+const employeeData = [
+  {
+    "employeeId": "EMP001",
+    "email": "rahul.sharma@company.com",
+    "password": "224755"
+  },
+  {
+    "employeeId": "EMP002",
+    "email": "priya.verma@company.com",
+    "password": "224755"
+  },
+  {
+    "employeeId": "EMP003",
+    "email": "aman.gupta@company.com",
+    "password": "224755"
+  },
+  {
+    "employeeId": "EMP004",
+    "email": "neha.singh@company.com",
+    "password": "224755"
+  },
+  {
+    "employeeId": "EMP005",
+    "email": "rohit.patel@company.com",
+    "password": "224755"
+  }
+]
+const adminData = [
+    {
+        "id" : "AD1",
+        "email" : "keshariyakrishna8@gmail.com",
+        "password" : "Kk224755"
+    }
+]
