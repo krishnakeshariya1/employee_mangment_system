c71c76df716817ff633f47ba27a7a95e6f849455
krishnakeshariya1 <keshariyakrishna8@gmail.com>
2026-03-08 19:14:23 +0530

    employee dashboard completed

--- Employee_mangment_system/src/Components/TaskList/AcceptTask.jsx
+++ Employee_mangment_system/src/Components/TaskList/AcceptTask.jsx
@@ -2,8 +2,8 @@ export const AcceptTask = ({ task }) => {
     return (
         <div className="bg-amber-400 h-full w-[350px] rounded-xl shrink-0 p-5 flex flex-col justify-around">
             <div className="flex justify-between items-center">
-                <h3 className="bg-red-600 w-fit py-1.5 px-3 rounded-lg font-semibold text-white">High</h3>
-                <h4 className="font-semibold">20 feb 2024</h4>
+                <h3 className="bg-red-600 w-fit py-1.5 px-3 rounded-lg font-semibold text-white">{task.taskCategory}</h3>
+                <h4 className="font-semibold">{task.taskDate}</h4>
             </div>
             <div>
                 <h2 className="mt-6 font-semibold text-xl">{task.taskTitle}</h2>
